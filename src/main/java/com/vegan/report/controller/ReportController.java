56c13d3a83c1550e9c7d3071c37472960d7efd1e
seoyoumin <asd324500@naver.com>
2023-05-15 16:32:39 +0900

    병합 수정

--- src/main/java/com/vegan/report/controller/ReportController.java
+++ src/main/java/com/vegan/report/controller/ReportController.java
@@ -21,7 +21,7 @@ import com.vegan.report.service.ReportService;
 public class ReportController {
 
 	Logger logger = LoggerFactory.getLogger(getClass());
-<<<<<<< HEAD
+
 	@Autowired
 	ReportService service;
 
@@ -35,20 +35,7 @@ public class ReportController {
 		model.addAttribute("reporter", reporter);
 		logger.info("board_title" + board_title + user_id + reporter);
 
-=======
-	@Autowired ReportService service;
-	@RequestMapping(value="/reportWrite.go")
-	public String reportWrite(
-			@RequestParam String board_id, Model model,@RequestParam String board_title,@RequestParam String cat_id, @RequestParam String user_id, @RequestParam String reporter) {
-		model.addAttribute("board_id",board_id);
-		model.addAttribute("board_title",board_title);
-		model.addAttribute("cat_id",cat_id);
-		model.addAttribute("user_id",user_id);
-		model.addAttribute("reporter",reporter);
-		/* model.addAttribute("comment_id",comment_id); */
-		logger.info("board_title"+board_title+user_id+reporter);
-		
->>>>>>> origin/master
+
 		return "reportWrite";
 	}
 
