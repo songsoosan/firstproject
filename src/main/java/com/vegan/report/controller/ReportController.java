7b4fcfed9efe91a9d93002b4e429250748245cd8
seoyoumin <asd324500@naver.com>
2023-05-16 10:48:23 +0900

    병합 수정

--- src/main/java/com/vegan/report/controller/ReportController.java
+++ src/main/java/com/vegan/report/controller/ReportController.java
@@ -25,21 +25,6 @@ public class ReportController {
 	@Autowired
 	ReportService service;
 
-	/*
-	 * @RequestMapping(value = "/reportWrite.go") public String
-	 * reportWrite(@RequestParam String board_id, Model model, @RequestParam String
-	 * board_title,
-	 * 
-	 * @RequestParam String cat_id, @RequestParam String user_id, @RequestParam
-	 * String reporter) { model.addAttribute("board_id", board_id);
-	 * model.addAttribute("board_title", board_title); model.addAttribute("cat_id",
-	 * cat_id); model.addAttribute("user_id", user_id);
-	 * model.addAttribute("reporter", reporter); logger.info("board_title" +
-	 * board_title + user_id + reporter);
-	 * 
-	 * 
-	 * return "reportWrite"; }
-	 */
 	
 	@RequestMapping(value = "/reportWrite.go")
 
